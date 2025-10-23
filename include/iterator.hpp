3e3c63ed8d1de834f1b84a0d42eb7b0401c147cd
grigor <musoyangrigor251@gmail.com>
2025-10-23 16:20:45 +0400

    Implement full hierarchy of custom iterator classes

---
+++ include/iterator.hpp
@@ -0,0 +1,203 @@
+#pragma once
+
+#include "iterator_tags.hpp"
+#include "iterator_base.hpp"
+
+// Input iterator
+template<typename T>
+class Input_iterator : public Iterator<input_iterator_tag, T> {
+public:
+    explicit Input_iterator(T *p = nullptr) : m_ptr(p) {
+    }
+
+    T &operator*() const {
+        return *m_ptr;
+    }
+
+    Input_iterator &operator++() {
+        ++m_ptr;
+        return *this;
+    }
+
+    Input_iterator &operator++(int) {
+        Input_iterator tmp = *this;
+        ++(*this);
+        return *tmp;
+    }
+
+    bool operator==(const Input_iterator &other) const {
+        return m_ptr == other.m_ptr;
+    }
+
+    bool operator!=(const Input_iterator &other) const {
+        return m_ptr != other.m_ptr;
+    }
+
+private:
+    T *m_ptr;
+};
+
+// Output iterator
+template<typename T>
+class Output_iterator : public Iterator<output_iterator_tag, T> {
+public:
+    explicit Output_iterator(T *p = nullptr) : m_ptr(p) {
+    }
+
+    Output_iterator &operator=(const T &value) {
+        *m_ptr = value;
+        return *this;
+    }
+
+    Output_iterator &operator*() const {
+        return *m_ptr;
+    }
+
+    Output_iterator &operator++() {
+        ++m_ptr;
+        return *this;
+    }
+
+    Output_iterator &operator++(int) {
+        Output_iterator tmp = *this;
+        ++(*this);
+        return *tmp;
+    }
+
+private:
+    T *m_ptr;
+};
+
+// Forward iterator
+template<typename T>
+class Forward_iterator : public Iterator<forward_iterator_tag, T> {
+public:
+    explicit Forward_iterator(T *p = nullptr) : m_ptr(p) {
+    }
+
+    T &operator*() const {
+        return *m_ptr;
+    }
+
+    Forward_iterator &operator++() {
+        ++m_ptr;
+        return *this;
+    }
+
+    Forward_iterator &operator++(int) {
+        Forward_iterator tmp = *this;
+        ++(*this);
+        return *tmp;
+    }
+
+    bool operator==(const Forward_iterator &other) const {
+        return m_ptr == other.m_ptr;
+    }
+
+    bool operator!=(const Forward_iterator &other) const {
+        return m_ptr != other.m_ptr;
+    }
+
+private:
+    T *m_ptr;
+};
+
+// Bidirectional iterator
+template<typename T>
+class Bidirectional_iterator : public Iterator<bidirectional_iterator_tag, T> {
+public:
+    explicit Bidirectional_iterator(T *p = nullptr) : m_ptr(p) {
+    }
+
+    T &operator*() const {
+        return *m_ptr;
+    }
+
+    Bidirectional_iterator &operator++() {
+        ++m_ptr;
+        return *this;
+    }
+
+    Bidirectional_iterator &operator++(int) {
+        Forward_iterator tmp = *this;
+        ++(*this);
+        return *tmp;
+    }
+
+    Bidirectional_iterator &operator--() {
+        --m_ptr;
+        return *this;
+    }
+
+    Bidirectional_iterator &operator--(int) {
+        Bidirectional_iterator tmp = *this;
+        --(*this);
+        return *tmp;
+    }
+
+    bool operator==(const Bidirectional_iterator &other) const {
+        return m_ptr == other.m_ptr;
+    }
+
+    bool operator!=(const Bidirectional_iterator &other) const {
+        return m_ptr != other.m_ptr;
+    }
+
+private:
+    T *m_ptr;
+};
+
+// Random access iterator
+template<typename T>
+class Random_access_iterator : public Iterator<random_access_iterator_tag, T> {
+public:
+    explicit Random_access_iterator(T *p = nullptr) : m_ptr(p) {
+    }
+
+    T &operator*() const {
+        return *m_ptr;
+    }
+
+    Random_access_iterator &operator++() {
+        ++m_ptr;
+        return *this;
+    }
+
+    Random_access_iterator &operator++(int) {
+        Random_access_iterator tmp = *this;
+        ++(*this);
+        return *tmp;
+    }
+
+    Random_access_iterator &operator--() {
+        --m_ptr;
+        return *this;
+    }
+
+    Random_access_iterator &operator--(int) {
+        Random_access_iterator tmp = *this;
+        --(*this);
+        return *tmp;
+    }
+
+    Random_access_iterator &operator+(std::ptrdiff_t n) const {
+        return Random_access_iterator(m_ptr + n);
+    }
+
+    Random_access_iterator &operator-(std::ptrdiff_t n) const {
+        return Random_access_iterator(m_ptr - n);
+    }
+
+    std::ptrdiff_t operator-(const Random_access_iterator &other) const {
+        return m_ptr - other.m_ptr;
+    }
+
+    T &operator[](std::ptrdiff_t n) const {
+        return m_ptr[n];
+    }
+
+    bool operator<=>(const Random_access_iterator &other) const = default;
+
+private:
+    T *m_ptr;
+};
