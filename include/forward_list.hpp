2849d8589e28a2adf3027a970e4ba04202f9a6ec
grigor <musoyangrigor251@gmail.com>
2025-10-25 08:42:37 +0400

    Implement Forward_list class with push_front, push_back, and clear_data methods

--- include/forward_list.hpp
+++ include/forward_list.hpp
@@ -1,8 +1,50 @@
-//
-// Created by grigor on 10/25/25.
-//
+#pragma once
 
-#ifndef DATASTRUCTURES_FORWARD_LIST_H
-#define DATASTRUCTURES_FORWARD_LIST_H
+#include <cstddef>
 
-#endif //DATASTRUCTURES_FORWARD_LIST_H
+#include "node.hpp"
+
+template <typename T>
+class Forward_list {
+public:
+    Forward_list() : m_head(nullptr), m_size(0) {}
+
+    ~Forward_list() {
+        clear_data();
+    }
+
+    void push_front(const T &value) {
+        auto new_node = new Node<T>(value);
+        new_node->next = m_head;
+        m_head = new_node;
+        ++m_size;
+    }
+
+    void push_back(const T &value) {
+        auto new_node = new Node<T>(value);
+       if (!m_head) {
+           m_head = new_node;
+       } else {
+            auto temp = m_head;
+            while (temp->next) {
+               temp = temp->next;
+           }
+           temp->next = new_node;
+       }
+        ++m_size;
+    }
+
+
+
+private:
+    Node<T>* m_head;
+    std::size_t m_size;
+
+    void clear_data() noexcept {
+        while (m_head) {
+            auto temp = m_head;
+            m_head = m_head->next;
+            delete temp;
+        }
+    }
+};
