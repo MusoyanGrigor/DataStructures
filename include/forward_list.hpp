dc3d9c5754d66db60fba222b0c3d31c49c37fede
grigor <musoyangrigor251@gmail.com>
2025-10-25 09:26:02 +0400

    Add range constructor overloads for Forward_list to support custom and standard iterators

--- include/forward_list.hpp
+++ include/forward_list.hpp
@@ -2,25 +2,24 @@
 
 #include <initializer_list>
 #include <ranges>
-
 #include "node.hpp"
 #include "iterator.hpp"
 
-template <typename T>
+template<typename T>
 class Forward_list {
 public:
     using value_type = T;
-    using pointer = T*;
-    using const_pointer = const T*;
-    using reference = T&;
-    using const_reference = const T&;
+    using pointer = T *;
+    using const_pointer = const T *;
+    using reference = T &;
+    using const_reference = const T &;
     using size_type = std::size_t;
     using iterator = Forward_iterator<T>;
     using const_iterator = Forward_iterator<const T>;
 
     Forward_list() : m_head(nullptr), m_size(0) {}
 
-    Forward_list(const Forward_list& other) : m_head(nullptr), m_size(0) {
+    Forward_list(const Forward_list &other) : m_head(nullptr), m_size(0) {
         auto current = other.m_head;
         while (current) {
             push_back(current->value);
@@ -28,15 +27,30 @@ public:
         }
     }
 
-    Forward_list(Forward_list&& other) noexcept : m_head(other.m_head), m_size(other.m_size) {
+    Forward_list(Forward_list &&other) noexcept : m_head(other.m_head), m_size(other.m_size) {
         other.m_head = nullptr;
         other.m_size = 0;
     }
 
-    Forward_list(std::initializer_list<T> i_list) {
-        for (auto it = i_list.crbegin(); it != i_list.crend(); ++it) {
-            push_front(*it);
+    template<typename InputIt, typename = std::enable_if_t<std::is_base_of_v<input_iterator_tag,
+        typename InputIt::iterator_category> > >
+    Forward_list(InputIt first, InputIt last) {
+        for (auto it = first; it != last; ++it) {
+            push_back(*it);
         }
+    }
+
+    template<std::input_iterator InputIt>
+    Forward_list(InputIt first, InputIt last) {
+        for (auto it = first; it != last; ++it) {
+            push_back(*it);
+        }
+    }
+
+
+    Forward_list(std::initializer_list<T> i_list) {
+        for (auto I: i_list)
+            push_back(I);
     }
 
     explicit Forward_list(const size_type count) {
@@ -45,13 +59,11 @@ public:
         }
     }
 
-    explicit Forward_list(const size_type count, const T& value) {
-        for (size_type i = 0; i < count; ++i) {
-            push_back(value);
-        }
+    explicit Forward_list(const size_type count, const T &value) {
+        for (size_type i = 0; i < count; ++i) push_back(value);
     }
 
-    Forward_list& operator=(const Forward_list& other) {
+    Forward_list &operator=(const Forward_list &other) {
         if (this != &other) {
             clear_data();
             auto current = other.m_head;
@@ -63,7 +75,7 @@ public:
         return *this;
     }
 
-    Forward_list& operator=(Forward_list&& other) noexcept {
+    Forward_list &operator=(Forward_list &&other) noexcept {
         if (this != &other) {
             clear_data();
             m_head = other.m_head;
@@ -74,11 +86,9 @@ public:
         return *this;
     }
 
-    Forward_list& operator=(std::initializer_list<T> i_list) {
+    Forward_list &operator=(std::initializer_list<T> i_list) {
         clear_data();
-        for (auto it = i_list.crbegin(); it != i_list.crend(); ++it) {
-            push_front(*it);
-        }
+        for (auto i: i_list) push_back(i);
         return *this;
     }
 
@@ -93,20 +103,20 @@ public:
 
     void push_back(const T &value) {
         auto new_node = new Node<T>(value);
-       if (!m_head) {
-           m_head = new_node;
-       } else {
+        if (!m_head) {
+            m_head = new_node;
+        } else {
             auto temp = m_head;
             while (temp->next) {
-               temp = temp->next;
-           }
-           temp->next = new_node;
-       }
+                temp = temp->next;
+            }
+            temp->next = new_node;
+        }
         ++m_size;
     }
 
 private:
-    Node<value_type>* m_head;
+    Node<value_type> *m_head;
     size_type m_size;
 
     void clear_data() noexcept {
