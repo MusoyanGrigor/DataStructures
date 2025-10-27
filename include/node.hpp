ac48506bc9a4fa59a177b05dc248a9bfa1a77695
grigor <musoyangrigor251@gmail.com>
2025-10-27 12:26:21 +0400

    Add constructors to Node to support value copy, move, and emplace

--- include/node.hpp
+++ include/node.hpp
@@ -2,8 +2,14 @@
 
 template<typename T>
 struct Node {
-    explicit Node(const T &value) : value(value), next(nullptr) {}
+    Node() = default;
+
+    explicit Node(const T& val) : value(val) {}
+    explicit Node(T&& val) : value(std::move(val)) {}
+
+    template<typename... Args>
+    explicit Node(Args&&... args) : value(std::forward<Args>(args)...) {}
 
     T value;
-    Node<T> *next;
+    Node* next = nullptr;
 };
