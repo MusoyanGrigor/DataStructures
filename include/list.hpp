4af8df8e5593955d98ee45701392214ccee13b8b
grigor <musoyangrigor251@gmail.com>
2025-10-28 12:04:15 +0400

    Add full set of constructors to List class

--- include/list.hpp
+++ include/list.hpp
@@ -1,12 +1,52 @@
 #pragma once
+#include <algorithm>
 #include <cstddef>
 
+#include "algorithm.hpp"
 #include "node.hpp"
 
 template<typename T>
 class List {
 public:
-    List() : m_head(nullptr), m_tail(nullptr), m_size(0) {
+    List() : m_head(nullptr), m_tail(nullptr), m_size(0) {}
+
+    List(const List& other) : List() {
+        auto temp = other.m_head;
+        while (temp) {
+            push_back(temp->value);
+            temp = temp->next;
+        }
+    }
+
+    List(List&& other) noexcept : m_head(other.m_head), m_tail(other.m_tail), m_size(other.m_size) {
+        other.m_head = nullptr;
+        other.m_tail = nullptr;
+        other.m_size = 0;
+    }
+
+    template<typename InputIt, typename = std::enable_if_t<it::is_iterator<InputIt>::value> >
+    List(InputIt first, InputIt last) : List() {
+        for (auto it = first; it != last; ++it) {
+            push_back(*it);
+        }
+    }
+
+    List(std::initializer_list<T> i_list) : List() {
+        for (auto& value : i_list) {
+            push_back(value);
+        }
+    }
+
+    explicit List(const std::size_t count) : List() {
+        for (std::size_t i = 0; i < count; ++i) {
+            push_back(T());
+        }
+    }
+
+    List(const std::size_t count, const T& value) : List() {
+        for (std::size_t i = 0; i < count; ++i) {
+            push_back(value);
+        }
     }
 
     ~List() {clear_data();}
