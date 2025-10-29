373e75dc043f5cc569cef6ad9d38f31c74190702
grigor <musoyangrigor251@gmail.com>
2025-10-29 13:58:40 +0400

    Add push_front and push_back

--- include/deque.hpp
+++ include/deque.hpp
@@ -7,16 +7,17 @@ template<typename T>
 class Deque {
 public:
     Deque() : m_map_capacity(4), m_num_blocks(0) {
-        m_map = new T*[m_map_capacity];
+        m_map = new T *[m_map_capacity];
         for (std::size_t i = 0; i < m_map_capacity; ++i) {
             m_map[i] = nullptr;
         }
 
         m_front_block = m_back_block = m_map_capacity / 2;
-        m_front_index = m_back_index = m_map_capacity / 2;
-
         m_map[m_front_block] = new T[BLOCK_SIZE];
         ++m_num_blocks;
+
+        m_front_index = BLOCK_SIZE / 2;
+        m_back_index = m_front_index - 1;
     }
 
     ~Deque() {
@@ -27,8 +28,24 @@ public:
         delete[] m_map;
     }
 
+    void push_front(const T &value) {
+        if (m_front_index == 0) {
+            allocate_block_front();
+            m_front_index = BLOCK_SIZE - 1;
+        } else { --m_front_index; }
+        m_map[m_front_block][m_front_index] = value;
+    }
+
+    void push_back(const T &value) {
+        if (m_back_index == BLOCK_SIZE - 1) {
+            allocate_block_back();
+            m_back_index = 0;
+        } else { ++m_back_index; }
+        m_map[m_back_block][m_back_index] = value;
+    }
+
 private:
-    T** m_map;
+    T **m_map;
     std::size_t m_map_capacity;
     std::size_t m_num_blocks;
     std::size_t m_front_block, m_back_block;
@@ -49,7 +66,7 @@ private:
 
     void expand_map() {
         const std::size_t new_capacity = m_map_capacity * 2;
-        T** new_map = new T*[new_capacity];
+        T **new_map = new T *[new_capacity];
         const std::size_t shift = new_capacity / 4;
         for (std::size_t i = 0; i < shift; ++i) {
             new_map[i] = nullptr;
