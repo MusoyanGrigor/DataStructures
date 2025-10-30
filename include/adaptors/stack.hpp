1daaa059ffaa4e43248d1ad8894bf5a4e3a5d8d6
grigor <musoyangrigor251@gmail.com>
2025-10-30 17:03:34 +0400

    Implement generic Stack class with container support
    - Added Stack template class with default Deque container.
    - Supports standard stack operations: push, pop, top, emplace, size, empty.
    - Implemented copy/move constructors and assignment operators.
    - Added member and non-member swap functions.a

--- include/adaptors/stack.hpp
+++ include/adaptors/stack.hpp
@@ -1,8 +1,81 @@
-//
-// Created by grigor on 10/30/25.
-//
+#pragma once
 
-#ifndef DATASTRUCTURES_STACK_H
-#define DATASTRUCTURES_STACK_H
+#include <stack>
 
-#endif //DATASTRUCTURES_STACK_H
+#include "sequence/deque.hpp"
+
+template<typename T, typename Container = Deque<T>>
+class Stack {
+public:
+    Stack() = default;
+
+    explicit Stack(const Container& container) {
+        m_container = container;
+    }
+
+    Stack(const Stack& other) {
+        m_container = other.m_container;
+    }
+
+    Stack(Stack&& other) noexcept {
+        m_container = std::move(other.m_container);
+    }
+
+    Stack& operator=(const Stack& other) {
+        if (this != &other) {
+            m_container = other.m_container;
+        }
+        return *this;
+    }
+
+    Stack& operator=(Stack&& other) noexcept {
+        if (this != &other) {
+            m_container = std::move(other.m_container);
+        }
+        return *this;
+    }
+
+    ~Stack() = default;
+
+    T& top() {
+        return m_container.back();
+    }
+
+    const T& top() const {
+        return m_container.back();
+    }
+
+    bool empty() const {
+        return m_container.empty();
+    }
+
+    std::size_t size() const {
+        return m_container.size();
+    }
+
+    template<typename U>
+    void push(U&& value) {
+        m_container.push_back(std::forward<U>(value));
+    }
+
+    void pop() {
+        m_container.pop_back();
+    }
+
+    template<typename... Args>
+    void emplace(Args&&... args) {
+        m_container.emplace_back(std::forward<Args>(args)...);
+    }
+
+    void swap(Stack& other) noexcept {
+        std::swap(m_container, other.m_container);
+    }
+
+private:
+    Container m_container;
+};
+
+template<typename T>
+void swap(Stack<T>& lhs, Stack<T>& rhs) noexcept {
+    lhs.swap(rhs);
+}
