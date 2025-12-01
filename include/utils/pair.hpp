28db297f8b2136c6a7cfff2fa21d255a22bfa1ce
grigor <musoyangrigor251@gmail.com>
2025-12-01 10:38:40 +0400

    Add make_pair helper function

--- include/utils/pair.hpp
+++ include/utils/pair.hpp
@@ -7,6 +7,7 @@ public:
     using first_type = T1;
     using second_type = T2;
 
+    // Constructors
     Pair() : m_first(), m_second() {}
 
     template <typename U1, typename U2>
@@ -15,6 +16,7 @@ public:
     Pair(T1&& value1, T2&& value2)
         : m_first(std::move(value1)), m_second(std::move(value2)) {}
 
+    // Assignment operator
     Pair& operator=(const Pair &other) {
         if (this != &other) {
             m_first = other.m_first;
@@ -31,6 +33,7 @@ public:
         return *this;
     }
 
+    // Accessors
     T1& first() { return m_first; }
     const T1& first() const { return m_first; }
 
@@ -43,6 +46,7 @@ public:
         swap(m_second, other.m_second);
     }
 
+    // Comparison operator
     auto operator<=>(const Pair&) const = default;
 
 private:
@@ -50,7 +54,14 @@ private:
     T2 m_second;
 };
 
+// Non-member swap
 template <typename T1, typename T2>
 void swap(Pair<T1, T2> &p1, Pair<T1, T2> &p2) noexcept {
     p1.swap(p2);
+}
+
+// make_pair helper
+template <typename T1, typename T2>
+constexpr Pair<std::decay_t<T1>, std::decay_t<T2>> make_pair(T1 &&p1, T2 &&p2) {
+    return Pair<std::decay_t<T1>, std::decay_t<T2>>(std::forward<T1>(p1), std::forward<T2>(p2));
 }
