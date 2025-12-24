e801fbaecb383816bd3355948509d375e9ffc1b5
grigor <musoyangrigor251@gmail.com>
2025-12-24 15:29:10 +0400

    Fix HashMapIterator to support const Map types

--- include/associative/internal/hash_map_iterator.hpp
+++ include/associative/internal/hash_map_iterator.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <type_traits>
 
 template <typename Map>
 class HashMapIterator {
@@ -7,10 +8,14 @@ public:
     using bucket_type = map_type::bucket_type;
     using value_type = map_type::value_type;
     using size_type = map_type::size_type;
-    using bucket_iterator = bucket_type::iterator;
+    using bucket_iterator = std::conditional_t<
+        std::is_const_v<Map>,
+        typename bucket_type::const_iterator,
+        typename bucket_type::iterator
+    >;
 
     // Constructor
-    HashMapIterator(map_type *map, size_type bucket_index, bucket_iterator it)
+    HashMapIterator(map_type* map, size_type bucket_index, bucket_iterator it)
         : m_map(map), m_bucket_index(bucket_index), m_it(it)
     {
         if (m_map && m_bucket_index < m_map->get_bucket_count() &&
@@ -20,11 +25,18 @@ public:
     }
 
     // Dereference
-    value_type &operator*() const { return *m_it; }
-    value_type *operator->() const { return &(*m_it); }
+    value_type& operator*() const { return *m_it; }
+
+    auto operator->() const -> std::conditional_t<
+        std::is_const_v<Map>,
+        const value_type*,
+        value_type*
+    > {
+        return &(*m_it);
+    }
 
     // Pre-increment
-    HashMapIterator &operator++() {
+    HashMapIterator& operator++() {
         ++m_it;
         if (m_map && (m_bucket_index < m_map->get_bucket_count()) &&
             m_it == m_map->get_buckets()[m_bucket_index].end()) {
@@ -41,29 +53,28 @@ public:
     }
 
     // Comparison
-    bool operator==(const HashMapIterator &other) const {
+    bool operator==(const HashMapIterator& other) const {
         return m_map == other.m_map &&
                m_bucket_index == other.m_bucket_index &&
                (m_bucket_index == end_bucket_index() || m_it == other.m_it);
     }
 
-    bool operator!=(const HashMapIterator &other) const {
+    bool operator!=(const HashMapIterator& other) const {
         return !(*this == other);
     }
 
     static constexpr size_type end_bucket_index() { return static_cast<size_type>(-1); }
 
 private:
-    map_type *m_map = nullptr;
+    map_type* m_map = nullptr;
     size_type m_bucket_index = 0;
     bucket_iterator m_it;
-
 
     void advance_to_next_bucket() {
         if (!m_map) return;
         ++m_bucket_index;
         while (m_bucket_index < m_map->get_bucket_count()) {
-            auto &bucket = m_map->get_buckets()[m_bucket_index];
+            auto& bucket = m_map->get_buckets()[m_bucket_index];
             if (!bucket.empty()) {
                 m_it = bucket.begin();
                 return;
