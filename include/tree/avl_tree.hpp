fe1face72e438c7a32ef8d6c787ae48e6556411f
grigor <musoyangrigor251@gmail.com>
2025-12-29 15:48:48 +0400

    Implement remove function with AVL rebalancing

--- include/tree/avl_tree.hpp
+++ include/tree/avl_tree.hpp
@@ -5,31 +5,32 @@
 #include "binary_search_tree.hpp"
 #include "internal/node.hpp"
 
-template <typename T>
+template<typename T>
 class AVL_tree {
 public:
     using value_type = T;
-    using pointer = T*;
-    using const_pointer = const T*;
-    using reference = T&;
-    using const_reference = const T&;
+    using pointer = T *;
+    using const_pointer = const T *;
+    using reference = T &;
+    using const_reference = const T &;
     using size_type = size_t;
 
     // Constructors
-    AVL_tree() : m_root(nullptr), m_size(0) {}
+    AVL_tree() : m_root(nullptr), m_size(0) {
+    }
 
-    AVL_tree(const AVL_tree& other) : AVL_tree() {
+    AVL_tree(const AVL_tree &other) : AVL_tree() {
         m_root = copy_nodes(other.m_root);
         m_size = other.m_size;
     }
 
-    AVL_tree(AVL_tree&& other) noexcept : m_root(other.m_root), m_size(other.m_size) {
+    AVL_tree(AVL_tree &&other) noexcept : m_root(other.m_root), m_size(other.m_size) {
         other.m_root = nullptr;
         other.m_size = 0;
     }
 
     // Assignment operator
-    AVL_tree& operator=(const AVL_tree& other) {
+    AVL_tree &operator=(const AVL_tree &other) {
         if (this != &other) {
             clear_data();
             m_root = copy_nodes(other.m_root);
@@ -38,7 +39,7 @@ public:
         return *this;
     }
 
-    AVL_tree& operator=(AVL_tree&& other) noexcept {
+    AVL_tree &operator=(AVL_tree &&other) noexcept {
         if (this != &other) {
             m_root = other.m_root;
             m_size = other.m_size;
@@ -48,12 +49,18 @@ public:
         return *this;
     }
 
+    // Destructor
     ~AVL_tree() {
         clear_data(m_root);
     }
 
+    // Modifiers
     void insert(const_reference value) {
         m_root = insert_node(m_root, value);
+    }
+
+    void remove(const_reference value) {
+        m_root = remove_node(m_root, value);
     }
 
     void clear() {
@@ -72,17 +79,17 @@ public:
     }
 
 private:
-    AVLNode<value_type>* m_root;
+    AVLNode<value_type> *m_root;
     size_type m_size;
 
-    void clear_data(AVLNode<value_type>* node) {
+    void clear_data(AVLNode<value_type> *node) {
         if (!node) return;
         clear_data(node->left);
         clear_data(node->right);
         delete node;
     }
 
-    AVLNode<value_type>* copy_nodes(AVLNode<value_type>* node) {
+    AVLNode<value_type> *copy_nodes(AVLNode<value_type> *node) {
         if (!node) return nullptr;
 
         auto new_node = new AVLNode<value_type>(node->value);
@@ -92,7 +99,7 @@ private:
         return new_node;
     }
 
-    AVLNode<value_type>* insert_node(AVLNode<value_type>* node, const_reference value) {
+    AVLNode<value_type> *insert_node(AVLNode<value_type> *node, const_reference value) {
         if (!node) {
             ++m_size;
             return new AVLNode<value_type>(value);
@@ -116,7 +123,7 @@ private:
         }
 
         // RR case
-        if (balance < - 1 && value > node->right->value) {
+        if (balance < -1 && value > node->right->value) {
             return rotate_left(node);
         }
 
@@ -135,15 +142,69 @@ private:
         return node;
     }
 
-    int get_height(AVLNode<value_type>* node) const {
+    AVLNode<value_type> *remove_node(AVLNode<value_type> *node, const_reference value) {
+        if (!node) return nullptr;
+
+        if (value < node->value) {
+            node->left = remove_node(node->left, value);
+        } else if (value > node->value) {
+            node->right = remove_node(node->right, value);
+        } else {
+            if (!node->left) {
+                auto rightChild = node->right;
+                delete node;
+                --m_size;
+                return rightChild;
+            }
+            if (!node->right) {
+                auto leftChild = node->left;
+                delete node;
+                --m_size;
+                return leftChild;
+            }
+
+            AVLNode<value_type> *minNode = node->right;
+            while (minNode->left) minNode = minNode->left;
+            node->value = minNode->value;
+            node->right = remove_node(node->right, minNode->value);
+        }
+
+        node->height = 1 + std::max(get_height(node->left), get_height(node->right));
+
+        const int balance = get_balance(node);
+
+        // LL case
+        if (balance > 1 && get_balance(node->left) >= 0)
+            return rotate_right(node);
+
+        // LR case
+        if (balance > 1 && get_balance(node->left) < 0) {
+            node->left = rotate_left(node->left);
+            return rotate_right(node);
+        }
+
+        // RR case
+        if (balance < -1 && get_balance(node->right) <= 0)
+            return rotate_left(node);
+
+        // RL case
+        if (balance < -1 && get_balance(node->right) > 0) {
+            node->right = rotate_right(node->right);
+            return rotate_left(node);
+        }
+
+        return node;
+    }
+
+    int get_height(AVLNode<value_type> *node) const {
         return node ? node->height : 0;
     }
 
-    int get_balance(AVLNode<value_type>* node) const {
+    int get_balance(AVLNode<value_type> *node) const {
         return node ? get_height(node->left) - get_height(node->right) : 0;
     }
 
-    AVLNode<value_type>* rotate_left(AVLNode<value_type>* x) {
+    AVLNode<value_type> *rotate_left(AVLNode<value_type> *x) {
         auto y = x->right;
         auto T2 = y->left;
 
@@ -156,9 +217,9 @@ private:
         return y;
     }
 
-    AVLNode<value_type>* rotate_right(AVLNode<value_type>* y) {
-        AVLNode<value_type>* x = y->left;
-        AVLNode<value_type>* T2 = x->right;
+    AVLNode<value_type> *rotate_right(AVLNode<value_type> *y) {
+        AVLNode<value_type> *x = y->left;
+        AVLNode<value_type> *T2 = x->right;
 
         x->right = y;
         y->left = T2;
@@ -168,5 +229,4 @@ private:
 
         return x;
     }
-
 };
