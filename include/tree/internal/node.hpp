39aeda7d53396c1a60e7060ca9514fd29a356f2f
grigor <musoyangrigor251@gmail.com>
2025-12-29 14:17:28 +0400

    Add AVL tree node

--- include/tree/internal/node.hpp
+++ include/tree/internal/node.hpp
@@ -8,3 +8,13 @@ struct TNode {
     TNode* left = nullptr;
     TNode* right = nullptr;
 };
+
+template <typename T>
+struct AVLNode {
+    explicit AVLNode(const T& val) : value(val) {}
+
+    T value;
+    AVLNode* left = nullptr;
+    AVLNode* right = nullptr;
+    int height = 1;
+};
